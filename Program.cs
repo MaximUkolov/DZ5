7f1149d3878ca8072fb5c6ca1aa911f5923bc599
Max <0665232@mail.ru>
2022-10-18 10:34:56 +0300

    Решена 38

--- Program.cs
+++ Program.cs
@@ -43,6 +43,40 @@
 // [-4, -6, 89, 6] -> 0З
 
 
+// int[] arr = new int[8];
+// int sum1 = 0;
+// int sum2 = 0;
+
+// void newArray(int[]array)
+// {
+//     for (int i=0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-50,50);
+//     }
+// }
+
+// void PrintArray(int[]array)
+// {
+//     for (int i=0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]}");
+//         if (i < (array.Length)) Console.Write($", ");
+
+//     }
+//     Console.WriteLine();
+// }
+
+// newArray(arr);
+// PrintArray(arr);
+// for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i % 2 == 0)
+//             sum1 = sum1 + arr[i];
+//         else
+//             sum2 = sum2 + arr[i];
+//     }
+
+// Console.WriteLine("Сумма элементов с нечетными номерами (нумерация начинается с нуля): {0} ", sum2);
 
 
 
@@ -51,3 +85,33 @@
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+
+
+int[] array = new int[8];
+void newArray(int[]array)
+{for (int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-50,50);
+    }}
+void PrintArray(int[]array)
+{for (int i=0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < (array.Length)) Console.Write($", ");    }
+    Console.WriteLine("  "); }
+newArray(array);
+PrintArray(array);
+int max = array[0];
+int min = array[0];
+for (int i=0; i < array.Length; i++)
+ {if (array[i] < min)
+    { 
+        min = array[i];
+    }
+    else if (array[i] > max)
+    {
+        max = array[i];   }}
+Console.WriteLine("Разница между макс и мин элементами: " +(max - min));
+
+
